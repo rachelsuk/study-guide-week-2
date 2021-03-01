e53fa73675560f743125f2510dc30b9490273734
Rachel <rachelsuk97@gmail.com>
2021-03-01 05:35:04 +0000

    Completed dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,178 @@
+"""Dictionaries Practice
+
+**IMPORTANT:** These problems are meant to be solved using
+dictionaries and sets.
+"""
+
+
+def without_duplicates(words):
+    """Given a list of words, return list with duplicates removed.
+
+    For example:
+
+        >>> no_dupes = without_duplicates(
+        ...     ["rose", "is", "a", "rose", "is", "a", "rose"])
+
+        >>> isinstance(no_dupes, list)
+        True
+
+        >>> sorted(without_duplicates(
+        ...     ["rose", "is", "a", "rose", "is", "a", "rose"]))
+        ['a', 'is', 'rose']
+
+    You should treat differently-capitalized words as different:
+
+        >>> sorted(without_duplicates(
+        ...     ["Rose", "is", "a", "rose", "is", "a", "rose"]))
+        ['Rose', 'a', 'is', 'rose']
+
+        An empty list should return an empty list:
+
+        >>> sorted(without_duplicates(
+        ...     []))
+        []
+
+    The function should work for a list containing integers:
+
+        >>> sorted(without_duplicates([111111, 2, 33333, 2]))
+        [2, 33333, 111111]
+
+    The function should return a variable of type list:
+        >>> type(without_duplicates([111111, 2, 33333, 2]))
+        <class 'list'>
+    """
+    no_duplicates = list(set(words))
+
+    return no_duplicates
+
+
+def find_unique_common_items(items1, items2):
+    """Produce the set of *unique* common items in two lists.
+
+    Given two lists, return a set of the *unique* common items
+    shared between the lists.
+
+    **IMPORTANT**: you may not use `'if ___ in ___``
+    or the method `list.index()`.
+
+    This should return a set:
+
+        >>> unique_common_items = find_unique_common_items([1, 2, 3, 4], [2, 1])
+        >>> isinstance(unique_common_items, set)
+        True
+
+    This should find [1, 2]:
+
+        >>> sorted(find_unique_common_items([1, 2, 3, 4], [2, 1]))
+        [1, 2]
+
+    However, now we only want unique items, so for these lists,
+    don't show more than 1 or 2 once:
+
+        >>> sorted(find_unique_common_items([3, 2, 1], [1, 1, 2, 2]))
+        [1, 2]
+
+    The elements should not be treated as duplicates if they are
+    different data types:
+
+        >>> sorted(find_unique_common_items(["2", "1", 2], [2, 1]))
+        [2]
+    """
+
+    return set(items1) & set(items2)
+
+
+def get_sum_zero_pairs(numbers):
+    """Given list of numbers, return list of pairs summing to 0.
+
+    Given a list of numbers, add up each individual pair of numbers.
+    Return a list of each pair of numbers that adds up to 0.
+
+    For example:
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1]) )
+        [[-2, 2], [-1, 1]]
+
+        >>> sort_pairs( get_sum_zero_pairs([3, -3, 2, 1, -2, -1]) )
+        [[-3, 3], [-2, 2], [-1, 1]]
+
+    This should always be a unique list, even if there are
+    duplicates in the input list:
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1]) )
+        [[-2, 2], [-1, 1]]
+
+    Of course, if there are one or more zeros to pair together,
+    that's fine, too (even a single zero can pair with itself):
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 3, -1, 1, 1, 0]) )
+        [[-1, 1], [0, 0]]
+    """
+    pairs = []
+    used_numbers = set()
+    for number_1 in numbers:
+        for number_2 in numbers:
+            if number_1 + number_2 == 0:
+                pair = [number_1, number_2]
+                if number_1 not in used_numbers:
+                    pairs.append(pair)
+                used_numbers.add(number_1)
+                used_numbers.add(number_2)
+
+    return pairs
+
+def top_chars(phrase):
+    """Find most common character(s) in string.
+
+    Given an input string, return a list of character(s) which
+    appear(s) the most in the input string.
+
+    If there is a tie, the order of the characters in the returned
+    list should be alphabetical.
+
+    For example:
+
+        >>> top_chars("The rain in spain stays mainly in the plain.")
+        ['i', 'n']
+
+    If there is not a tie, simply return a list with one item.
+
+    For example:
+
+        >>> top_chars("Shake it off, shake it off.")
+        ['f']
+
+    Do not count spaces, but count all other characters.
+
+    """
+    char_dict = {}
+
+    words = phrase.split(" ")
+    for word in words:
+        for letter in word:
+            char_dict[letter.lower()] = char_dict.get(letter.lower(), 0) + 1
+
+    greatest_number = 0
+    for key, value in char_dict.items():
+        if value > greatest_number:
+            greatest_number = value
+
+    return [key for key, value in char_dict.items() if value == greatest_number]
+
+#####################################################################
+# You can ignore everything below this.
+
+
+def sort_pairs(l):
+    # Print sorted list of pairs where the pairs are sorted. This is
+    # used only for documentation tests. You can ignore it.
+    return sorted(sorted(pair) for pair in l)
+
+
+if __name__ == "__main__":
+    print()
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("*** ALL TESTS PASSED ***")
+    print()
+
